7def766d76c70491d428296a2a141e0d71f92306
zhanyeye <zhanyeye@qq.com>
2019-11-12 22:43:54 +0800

    Initial commit

---
+++ src/main/java/com/softeng/dingtalk/DingtalkApplication.java
@@ -0,0 +1,13 @@
+package com.softeng.dingtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingtalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DingtalkApplication.class, args);
+    }
+
+}
