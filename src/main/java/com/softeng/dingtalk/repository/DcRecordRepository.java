8d4aebf6ee35649939886122f70feef5f2aa32e3
zhanyeye <zhanyeye@qq.com>
2019-12-30 16:43:49 +0800

    update DcRecordRepository

--- src/main/java/com/softeng/dingtalk/repository/DcRecordRepository.java
+++ src/main/java/com/softeng/dingtalk/repository/DcRecordRepository.java
@@ -1,7 +1,9 @@
 package com.softeng.dingtalk.repository;
 
         import com.softeng.dingtalk.entity.DcRecord;
+        import org.springframework.data.domain.Pageable;
         import org.springframework.data.jpa.repository.JpaRepository;
+        import org.springframework.data.jpa.repository.Modifying;
         import org.springframework.data.jpa.repository.Query;
         import org.springframework.data.repository.query.Param;
         import org.springframework.stereotype.Repository;
@@ -15,6 +17,35 @@ package com.softeng.dingtalk.repository;
  */
 @Repository
 public interface DcRecordRepository extends CustomizedRepository<DcRecord, Integer>, JpaRepository<DcRecord, Integer> {
+    /**
+     * 用于分页显示申请历史 ->  根据uid(用户)，获取用户提交的申请，实现分页
+     * @param uid 申请人ID
+     * @param pageable
+     * @return java.util.List<com.softeng.dingtalk.entity.DcRecord>
+     * @Date 4:28 PM 12/30/2019
+     **/
+    @Query("select d from DcRecord d where d.applicant.id = :uid")
+    List<DcRecord> listByUid(@Param("uid") int uid, Pageable pageable);
+
+    /**
+     * 审核人查看待审核的申请  ->  根据uid(审核人)，获得待审核的申请
+     * @param uid  审核人id
+     * @return java.util.List<com.softeng.dingtalk.entity.DcRecord>
+     * @Date 4:36 PM 12/30/2019
+     **/
+    @Query("select d from DcRecord d where d.auditor.id = :uid and d.ischeck = false")
+    List<DcRecord> listPendingReview(@Param("uid") int uid);
+
+    /**
+     * 更新申请状态为已审核
+     * @param id  申请id
+     * @return void
+     * @Date 4:40 PM 12/30/2019
+     **/
+    @Modifying
+    @Query("update DcRecord d set d.ischeck = true where d.id = :id")
+    void updateCheckStatus(@Param("id") int id);
+    
     /**
      * 计算指定用户，指定周的，在各组的dc之和
      * @param uid 用户ID
