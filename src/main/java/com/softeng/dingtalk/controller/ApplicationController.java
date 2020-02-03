3f5aaa81b817b4953cc58dbc53748eb1a809bffe
zhanyeye <zhanyeye@qq.com>
2020-02-03 16:46:11 +0800

    更新获取时间的返回值和提交申请代码

--- src/main/java/com/softeng/dingtalk/controller/ApplicationController.java
+++ src/main/java/com/softeng/dingtalk/controller/ApplicationController.java
@@ -2,16 +2,13 @@ package com.softeng.dingtalk.controller;
 
 import com.softeng.dingtalk.component.DingTalkUtils;
 import com.softeng.dingtalk.component.Utils;
-import com.softeng.dingtalk.dto.ApplicationDTO;
-import com.softeng.dingtalk.entity.AcItem;
 import com.softeng.dingtalk.entity.DcRecord;
-import com.softeng.dingtalk.entity.User;
 import com.softeng.dingtalk.repository.DcSummaryRepository;
 import com.softeng.dingtalk.service.ApplicationService;
 import com.softeng.dingtalk.service.AuditService;
 import com.softeng.dingtalk.service.UserService;
+import com.softeng.dingtalk.vo.ApplicationVO;
 import com.softeng.dingtalk.vo.DcSummaryVO;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Map;
 
@@ -53,14 +50,13 @@ public class ApplicationController {
 
     /**
      * 返回请求中的时间时本月第几周
-     * @param map
-     * @return java.util.Map
-     * @Date 2:17 PM 1/29/2020
+     * @param date
+     * @return int[] 数组大小为2，第一个时yearmonth, 第二个时week
+     * @Date 4:44 PM 2/3/2020
      **/
     @PostMapping("/getdate")
-    public Map getdate(@RequestBody Map<String, LocalDate> map) {
-        log.debug(map.get("time").toString());
-        return utils.getTimeFlag(map.get("time"));
+    public int[] getdate(@RequestBody LocalDate date) {
+        return utils.getTimeFlag(date);
     }
 
 
@@ -71,16 +67,12 @@ public class ApplicationController {
      * @Date 7:01 PM 12/27/2019
      **/
     @PostMapping("/application")
-    public void addApplication(@RequestBody ApplicationDTO applicationDTO, @RequestAttribute int uid) {
-        DcRecord dcRecord = applicationDTO.getDcRecord();// 获取DC申请信息
-        dcRecord.setApplicant(new User(uid)); //设置申请人
-        List<AcItem> acItems = applicationDTO.getAcItems(); //拿到 ac申请
-        int aid = dcRecord.getAuditor().getId(); //审核人id
-        Map<String, Integer> date = utils.getTimeFlag(applicationDTO.getDate());
-        dcRecord.setYearmonth(date.get("yearmonth"));
-        dcRecord.setWeek(date.get("week"));
-        if (applicationService.isExist(uid, aid, dcRecord.getYearmonth(), dcRecord.getWeek()) == false) {
-            applicationService.addApplication(dcRecord, acItems);    //持久化绩效申请
+    public void addApplication(@RequestAttribute int uid, @RequestBody ApplicationVO application) {
+        int[] result = utils.getTimeFlag(application.getDate()); //数组大小为2，result[0]: yearmonth, result[1] week
+        DcRecord dc = new DcRecord(application, uid, result[0], result[1]);
+
+        if (!applicationService.isExist(uid, application.getAuditorid(), result[0], result[1])) {
+            applicationService.addApplication(dc, application.getAcItems());    //持久化绩效申请
         } else {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "每周只能向同一个审核人提交一次申请");
         }
