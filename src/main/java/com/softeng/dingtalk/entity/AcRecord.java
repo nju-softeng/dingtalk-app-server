f754bef899c4c45396ad0db1ba758aebd7439117
zhanyeye <zhanyeye@qq.com>
2020-02-28 23:35:52 +0800

    update

--- src/main/java/com/softeng/dingtalk/entity/AcRecord.java
+++ src/main/java/com/softeng/dingtalk/entity/AcRecord.java
@@ -17,12 +17,19 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor
 public class AcRecord {
+
+    public static final int NORMAL = 0;
+    public static final int PROJECT = 1;
+    public static final int PAPER = 2;
+    public static final int VOTE = 3;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(columnDefinition="DECIMAL(10,3)")
     private double ac;
     private String reason;
+    private int classify;
     @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", updatable = false, insertable = false)
     private LocalDateTime createTime;
     @ManyToOne(fetch = FetchType.LAZY) //设置many端对one端延时加载，仅需要其ID
