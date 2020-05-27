e48a010648e74c128df2339e1ac476524f123e60
zhanyeye <zhanyeye@qq.com>
2020-05-28 07:36:26 +0800

    add  @JsonValue

--- src/main/java/com/softeng/dingtalk/controller/TestController.java
+++ src/main/java/com/softeng/dingtalk/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.softeng.dingtalk.controller;
 
+import com.softeng.dingtalk.entity.Test;
 import com.softeng.dingtalk.enums.Position;
+import com.softeng.dingtalk.repository.TestRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class TestController {
+
+    @Autowired
+    TestRepository testRepository;
+
     @GetMapping("/test/enums")
     public Position test_enums() {
         return Position.DOCTOR;
+    }
+
+    @GetMapping("/test/query_enums")
+    public Test getTest() {
+        return testRepository.findById(1).get();
+    }
+
+
+    @GetMapping("/test/query_enums_str")
+    public String getTest1() {
+        return testRepository.test();
     }
 
 }
---
+++ src/main/java/com/softeng/dingtalk/entity/Test.java
@@ -0,0 +1,29 @@
+package com.softeng.dingtalk.entity;
+
+import com.softeng.dingtalk.enums.Position;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author zhanyeye
+ * @description
+ * @create 5/28/2020 6:20 AM
+ */
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private Position position = Position.UNDERGRADUATE;
+
+}
