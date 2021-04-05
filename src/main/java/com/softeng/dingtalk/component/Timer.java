3467583b92bee81e921978cfeb424eb0bf87f26b
zhanyeye <zhanyeye@qq.com>
2021-04-05 15:53:16 +0800

    refactor DingTalkUtils

--- src/main/java/com/softeng/dingtalk/component/Timer.java
+++ src/main/java/com/softeng/dingtalk/component/Timer.java
@@ -62,7 +62,10 @@ public class Timer {
                 ExternalPaper externalPaper = externalPaperRepository.findByVid(v.getId());
                 if (externalPaper != null) {
                     // 发送投票开始的消息
-                    dingTalkUtils.sendVoteMsg(externalPaper.getId(),true, externalPaper.getTitle(), v.getEndTime().toLocalTime().toString(), null);
+                    String markdown = new StringBuilder(" #### 投票 \n ##### 论文： ").append(externalPaper.getTitle())
+                            .append(" \n 截止时间: ").append(v.getEndTime().toLocalTime().toString()).toString();
+                    String url = new StringBuilder().append("/paper/ex-detail/").append(externalPaper.getId()).append("/vote").toString();
+                    dingTalkUtils.sendActionCard("外部评审投票", markdown, "前往投票", url);
                 }
             }
         }
@@ -83,20 +86,25 @@ public class Timer {
             for (Vote v : votes) {
                 // 更新汇总投票结果
                 v = voteService.updateVote(v);
-                // 发送投票消息
+
+                String title ="", url = "";
                 if (v.isExternal()) {
-                    // 如果是外部投票
                     ExternalPaper externalPaper = externalPaperRepository.findByVid(v.getId());
-                    if (externalPaper != null) {
-                        dingTalkUtils.sendVoteResult(externalPaper.getId(), externalPaper.getTitle(), v.getResult(), v.getAccept(), v.getTotal(), v.isExternal());
-                    }
+                    title = externalPaper.getTitle();
+                    url = new StringBuilder().append("/paper/ex-detail/").append(externalPaper.getId()).append("/vote").toString();
                 } else {
-                    // 如果是内部投票
-                    Paper internalpaper = paperRepository.findByVid(v.getId());
-                    if (internalpaper != null) {
-                        dingTalkUtils.sendVoteResult(internalpaper.getId(), internalpaper.getTitle(), v.getResult(), v.getAccept(), v.getTotal(), v.isExternal());
-                    }
+                    Paper paper = paperRepository.findByVid(v.getId());
+                    title = paper.getTitle();
+                    url =new StringBuilder().append("/paper/in-detail/").append(paper.getId()).append("/vote").toString();
                 }
+
+                String markdown = new StringBuilder().append(" #### 投票结果 \n ##### 论文： ").append(title)
+                        .append(" \n 最终结果： ").append(v.getResult() ? "Accept" : "reject")
+                        .append("  \n  Accept: ").append(v.getAccept()).append(" 票  \n ")
+                        .append("Reject: ").append(v.getTotal() - v.getAccept()).append(" 票  \n ")
+                        .append("已参与人数： ").append(v.getTotal()).append("人  \n ").toString();
+
+                dingTalkUtils.sendActionCard("投票结果", markdown, "查看详情", url);
 
             }
         }
