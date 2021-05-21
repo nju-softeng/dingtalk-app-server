8406a3593c50abf411ac0f48bdbc72b2c58dc89a
zhanyeye <zhanyeye@qq.com>
2021-05-21 11:58:56 +0800

    update Timer.java

--- src/main/java/com/softeng/dingtalk/component/Timer.java
+++ src/main/java/com/softeng/dingtalk/component/Timer.java
@@ -3,6 +3,7 @@ package com.softeng.dingtalk.component;
 import com.softeng.dingtalk.api.MessageApi;
 import com.softeng.dingtalk.entity.ExternalPaper;
 import com.softeng.dingtalk.entity.InternalPaper;
+import com.softeng.dingtalk.entity.Paper;
 import com.softeng.dingtalk.entity.Vote;
 import com.softeng.dingtalk.repository.ExternalPaperRepository;
 import com.softeng.dingtalk.repository.InternalPaperRepository;
@@ -44,72 +45,63 @@ public class Timer {
     MessageApi messageApi;
 
     /**
-     * 每分钟扫描一次，看是否有待启动的投票，或者待结束的投票
+     * 每分钟扫描一次，看是否有待启动的投票
      */
     @Scheduled(cron = "0/20 * * * * ?")
     public void checkVote() {
         LocalDateTime now = LocalDateTime.now();
-//        log.debug("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-//        log.debug("checkout1: " + now.toString());
-//        log.debug("!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
 
         // (针对外部评审投票) 检查是否有投票需要开始
         List<Vote> upcomingVotes = voteService.listUpcomingVote(now);
-        if (upcomingVotes.size() != 0) {
-            for (Vote v : upcomingVotes) {
-                // 标注该投票已经开始
-                v.setStarted(true);
-                voteRepository.save(v);
+        upcomingVotes.forEach(v -> {
+            // 标注该投票已经开始
+            v.setStarted(true);
+            voteRepository.save(v);
 
-                ExternalPaper externalPaper = externalPaperRepository.findByVid(v.getId());
-                if (externalPaper != null) {
-                    // 发送投票开始的消息
-                    String markdown = new StringBuilder(" #### 投票 \n ##### 论文： ").append(externalPaper.getTitle())
-                            .append(" \n 截止时间: ").append(v.getEndTime().toLocalTime().toString()).toString();
-                    String url = new StringBuilder().append("/paper/ex-detail/").append(externalPaper.getId()).append("/vote").toString();
-                    messageApi.sendActionCard("外部评审投票", markdown, "前往投票", url);
-                }
+            ExternalPaper externalPaper = externalPaperRepository.findByVid(v.getId());
+            if (externalPaper != null) {
+                // 发送投票开始的消息
+                String markdown = " #### 投票 \n ##### 论文： " + externalPaper.getTitle() + " \n 截止时间: " + v.getEndTime().toLocalTime().toString();
+                String url = generateVoteDetailUrl(true, externalPaper.getId());
+                messageApi.sendActionCard("外部评审投票", markdown, "前往投票", url);
             }
-        }
+        });
     }
 
 
+    /**
+     * 每分钟扫描一次，看是否有待结束的投票
+     */
     @Scheduled(cron = "0/20 * * * * ?")
     public void checkVote2() {
         // 检查是否有需要被结束的投票
         LocalDateTime now = LocalDateTime.now();
 
-//        log.debug("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-//        log.debug("checkout2: " + now.toString());
-//        log.debug("!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
-
         List<Vote> votes = voteService.listUnderwayVote(now);
-        if (votes.size() != 0) {
-            for (Vote v : votes) {
-                // 更新汇总投票结果
-                v = voteService.updateVote(v);
+        votes.forEach(v -> {
+            // 更新汇总投票结果
+            v = voteService.updateVote(v);
 
-                String title ="", url = "";
-                if (v.isExternal()) {
-                    ExternalPaper externalPaper = externalPaperRepository.findByVid(v.getId());
-                    title = externalPaper.getTitle();
-                    url = new StringBuilder().append("/paper/ex-detail/").append(externalPaper.getId()).append("/vote").toString();
-                } else {
-                    InternalPaper internalPaper = internalPaperRepository.findByVid(v.getId());
-                    title = internalPaper.getTitle();
-                    url =new StringBuilder().append("/paper/in-detail/").append(internalPaper.getId()).append("/vote").toString();
-                }
+            Paper paper = v.isExternal() ? externalPaperRepository.findByVid(v.getId()) : internalPaperRepository.findByVid(v.getId());
 
-                String markdown = new StringBuilder().append(" #### 投票结果 \n ##### 论文： ").append(title)
-                        .append(" \n 最终结果： ").append(v.getResult() ? "Accept" : "reject")
-                        .append("  \n  Accept: ").append(v.getAccept()).append(" 票  \n ")
-                        .append("Reject: ").append(v.getTotal() - v.getAccept()).append(" 票  \n ")
-                        .append("已参与人数： ").append(v.getTotal()).append("人  \n ").toString();
+            String url = generateVoteDetailUrl(paper.isExternal(), paper.getId());
+            String markdown = voteResultInfo(paper.getTitle(), v.getResult(), v.getAccept(), v.getTotal());
 
-                messageApi.sendActionCard("投票结果", markdown, "查看详情", url);
+            messageApi.sendActionCard("投票结果", markdown, "查看详情", url);
 
-            }
-        }
+        });
+    }
+
+    private String generateVoteDetailUrl(boolean isExternal, int pid) {
+        return (isExternal ? "/paper/ex-detail/" : "/paper/in-detail/") + pid + "/vote";
+    }
+
+    private String voteResultInfo(String title, boolean result, int acceptCnt, int totalCnt) {
+        return new StringBuilder().append(" #### 投票结果 \n ##### 论文： ").append(title)
+                .append(" \n 最终结果： ").append(result ? "Accept" : "reject")
+                .append("  \n  Accept: ").append(acceptCnt).append(" 票  \n ")
+                .append("Reject: ").append(totalCnt - acceptCnt).append(" 票  \n ")
+                .append("已参与人数： ").append(totalCnt).append("人  \n ").toString();
     }
 
 
