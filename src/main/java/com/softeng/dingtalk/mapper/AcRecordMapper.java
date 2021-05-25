13000e27cddca7b68720941ce890e2b2b87c4a5d
zhanyeye <zhanyeye@qq.com>
2021-05-25 22:55:32 +0800

    add EasyExcel

---
+++ src/main/java/com/softeng/dingtalk/mapper/AcRecordMapper.java
@@ -0,0 +1,13 @@
+package com.softeng.dingtalk.mapper;
+
+import com.softeng.dingtalk.excel.AcData;
+import org.springframework.stereotype.Repository;
+
+import java.time.Month;
+import java.time.Year;
+import java.util.List;
+
+@Repository
+public interface AcRecordMapper {
+    List<AcData> listAcDataByYearMonth(String year, String month);
+}
