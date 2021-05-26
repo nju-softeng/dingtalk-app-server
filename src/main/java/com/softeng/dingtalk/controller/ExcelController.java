e14859056a3ef866959ac6b57434a6313ce66cc9
zhanyeye <zhanyeye@qq.com>
2021-05-26 22:59:34 +0800

    Revert "Merge branch 'dev'"
    This reverts commit b4d3d47a48cf14429d8e3245259f8051896db428, reversing
    changes made to 111a7c2ab08d1dc99ef89317f7cc8552939837a7.

--- src/main/java/com/softeng/dingtalk/controller/ExcelController.java
+++ src/main/java/com/softeng/dingtalk/controller/ExcelController.java
@@ -1,8 +1,5 @@
 package com.softeng.dingtalk.controller;
 
-import com.alibaba.excel.EasyExcel;
-import com.softeng.dingtalk.excel.AcData;
-import com.softeng.dingtalk.mapper.AcRecordMapper;
 import com.softeng.dingtalk.service.ExcelService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,22 +10,22 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.time.LocalDate;
 
+
 @Slf4j
 @RestController
 @RequestMapping("/api")
 public class ExcelController {
 
     @Autowired
-    AcRecordMapper acRecordMapper;
+    ExcelService excelService;
 
     @PostMapping("/excel/ac_data")
     public void downAcData(@RequestBody LocalDate date, HttpServletResponse response) throws IOException {
         response.setContentType("application/octet-stream");
         response.setCharacterEncoding("utf-8");
-        response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + title + ".xlsx");
-
-        List<AcData> acDataList = acRecordMapper.listAcDataByYearMonth(date.getYear(), date.getMonth());
-
+        // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
+        String fileName = URLEncoder.encode("测试", "UTF-8").replaceAll("\\+", "%20");
+        response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + fileName + ".xlsx");
         try {
             excelService.WriteAcDataByDate(date, response.getOutputStream());
         } catch (Exception e) {
@@ -38,5 +35,6 @@ public class ExcelController {
             response.setCharacterEncoding("utf-8");
             response.getWriter().println("下载文件失败: " + e.getMessage());
         }
+
     }
 }
