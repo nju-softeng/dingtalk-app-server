c43901100a538b6fabd494e92a699a866a92b723
zhanyeye <zhanyeye@qq.com>
2021-05-26 18:01:17 +0800

    fix bug for dingtalk app download

--- src/main/java/com/softeng/dingtalk/controller/ExcelController.java
+++ src/main/java/com/softeng/dingtalk/controller/ExcelController.java
@@ -9,8 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @Slf4j
 @RestController
@@ -22,7 +21,7 @@ public class ExcelController {
 
     @PostMapping("/excel/ac_data")
     public void downAcData(@RequestBody LocalDate date, HttpServletResponse response) throws IOException {
-        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.setContentType("application/ octet-stream");
         response.setCharacterEncoding("utf-8");
         // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
         String fileName = URLEncoder.encode("测试", "UTF-8").replaceAll("\\+", "%20");
