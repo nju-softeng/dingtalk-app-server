17408d93a1089368e0e0882b232dbf8a95c138f6
zhanyeye <zhanyeye@qq.com>
2021-05-26 23:01:55 +0800

    Update ExcelController.java

--- src/main/java/com/softeng/dingtalk/controller/ExcelController.java
+++ src/main/java/com/softeng/dingtalk/controller/ExcelController.java
@@ -1,5 +1,8 @@
 package com.softeng.dingtalk.controller;
 
+import com.alibaba.excel.EasyExcel;
+import com.softeng.dingtalk.excel.AcData;
+import com.softeng.dingtalk.mapper.AcRecordMapper;
 import com.softeng.dingtalk.service.ExcelService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.time.LocalDate;
-
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -17,24 +22,26 @@ import java.time.LocalDate;
 public class ExcelController {
 
     @Autowired
-    ExcelService excelService;
+    AcRecordMapper acRecordMapper;
 
     @PostMapping("/excel/ac_data")
     public void downAcData(@RequestBody LocalDate date, HttpServletResponse response) throws IOException {
-        response.setContentType("application/octet-stream");
+        String title = date.toString().substring(0, 7);
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
         response.setCharacterEncoding("utf-8");
-        // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
-        String fileName = URLEncoder.encode("测试", "UTF-8").replaceAll("\\+", "%20");
-        response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + fileName + ".xlsx");
+        response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + title + ".xlsx");
+
+        List<AcData> acDataList = acRecordMapper.listAcDataByYearMonth(date.getYear(), date.getMonth());
+
         try {
-            excelService.WriteAcDataByDate(date, response.getOutputStream());
-        } catch (Exception e) {
-            log.info("导出excel失败：{}", e.getMessage());
+            EasyExcel.write(response.getOutputStream(), AcData.class).sheet("模板").doWrite(acDataList);
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.info("导出excel失败{}", e.getMessage());
             response.reset();
             response.setContentType("application/json");
             response.setCharacterEncoding("utf-8");
             response.getWriter().println("下载文件失败: " + e.getMessage());
         }
-
     }
 }
