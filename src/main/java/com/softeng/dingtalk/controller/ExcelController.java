41114ed555d5fcd50cf35a793a80064af031fba1
zhanyeye <zhanyeye@qq.com>
2021-05-26 19:31:12 +0800

    Test download by get method

--- src/main/java/com/softeng/dingtalk/controller/ExcelController.java
+++ src/main/java/com/softeng/dingtalk/controller/ExcelController.java
@@ -20,9 +20,11 @@ public class ExcelController {
     @Autowired
     ExcelService excelService;
 
-    @PostMapping("/excel/ac_data")
-    public void downAcData(@RequestBody LocalDate date, HttpServletResponse response) throws IOException {
-        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+
+    @GetMapping("/excel/ac_data")
+    public void downAcData(@RequestParam(name = "date") String dateStr, HttpServletResponse response) throws IOException {
+        LocalDate date = LocalDate.parse(dateStr);
+        response.setContentType("application/octet-stream");
         response.setCharacterEncoding("utf-8");
         // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
         String fileName = URLEncoder.encode("测试", "UTF-8").replaceAll("\\+", "%20");
