b022884338367d9c294b1a6e26a523e52c80760e
zhanzeye <zhanyeye@qq.com>
2021-11-09 20:10:31 +0800

    style(service/test): rename & add Comments

--- src/test/java/com/softeng/dingtalk/service/WeeklyReportServiceTest.java
+++ src/test/java/com/softeng/dingtalk/service/WeeklyReportServiceTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 /**
@@ -34,36 +33,31 @@ public class WeeklyReportServiceTest {
     }
 
     @Test
-    public void testQueryUnsubmittedWeeklyReportUser() {
+    public void testQueryUnSubmittedWeeklyReportUser() {
         // 1. 10.11 - 10.17 未按时交周报的同学
-        System.out.println("10.11 - 10.17 未按时交周报的同学");
-        var users = weeklyReportService.queryUnsubmittedWeeklyReportUser(
+        log.info("10.11 - 10.17 未按时交周报的同学");
+        var users = weeklyReportService.queryUnSubmittedWeeklyReportUser(
                 LocalDateTime.of(2021, 10, 11, 0, 0, 0),
                 LocalDateTime.of(2021, 10, 18, 0, 0, 0));
         for(var user : users) {
             System.out.println(user.getName());
         }
         // 2. 10.18 - 10.24 未按时交周报的同学
-        System.out.println("10.18 - 10.24 未按时交周报的同学");
-        users = weeklyReportService.queryUnsubmittedWeeklyReportUser(
+        log.info("10.18 - 10.24 未按时交周报的同学");
+        users = weeklyReportService.queryUnSubmittedWeeklyReportUser(
                 LocalDateTime.of(2021, 10, 18, 0, 0, 0),
                 LocalDateTime.of(2021, 10, 25, 0, 0, 0));
         for(var user : users) {
             System.out.println(user.getName());
         }
         // 2. 10.25 - 10.31 未按时交周报的同学
-        System.out.println("10.25 - 10.31 未按时交周报的同学");
-        users = weeklyReportService.queryUnsubmittedWeeklyReportUser(
+        log.info("10.25 - 10.31 未按时交周报的同学");
+        users = weeklyReportService.queryUnSubmittedWeeklyReportUser(
                 LocalDateTime.of(2021, 10, 25, 0, 0, 0),
                 LocalDateTime.of(2021, 11, 1, 0, 0, 0));
         for(var user : users) {
-            System.out.println(user.getName());
+            log.info(user.getName());
         }
     }
 
-//    @Test
-//    public boolean testQueryUnsubmittedWeeklyReportUser(LocalDate date) {
-//
-////        weeklyReportService.
-//    }
 }
