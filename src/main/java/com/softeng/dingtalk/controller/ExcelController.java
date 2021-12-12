1d0c799298584089972a2b9cc4a17a5d8795934c
zhanyeye <zhanyeye@qq.com>
2021-12-12 10:49:35 +0800

    feat(DcSummaryData): export DcSummaryData excel by server

--- src/main/java/com/softeng/dingtalk/controller/ExcelController.java
+++ src/main/java/com/softeng/dingtalk/controller/ExcelController.java
@@ -1,8 +1,5 @@
 package com.softeng.dingtalk.controller;
 
-import com.alibaba.excel.EasyExcel;
-import com.softeng.dingtalk.excel.AcData;
-import com.softeng.dingtalk.mapper.AcRecordMapper;
 import com.softeng.dingtalk.service.ExcelService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URLEncoder;
+import java.io.OutputStream;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
@@ -22,26 +16,16 @@ import java.util.Map;
 public class ExcelController {
 
     @Autowired
-    AcRecordMapper acRecordMapper;
+    ExcelService excelService;
 
     @PostMapping("/excel/ac_data")
-    public void downAcData(@RequestBody LocalDate date, HttpServletResponse response) throws IOException {
-        String title = date.toString().substring(0, 7);
-        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-        response.setCharacterEncoding("utf-8");
-        response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + title + ".xlsx");
+    public void downloadAcData(@RequestBody LocalDate date, HttpServletResponse response) throws IOException {
+        excelService.writeAcDataByDate(date, response.getOutputStream());
+    }
 
-        List<AcData> acDataList = acRecordMapper.listAcDataByYearMonth(date.getYear(), date.getMonth());
 
-        try {
-            EasyExcel.write(response.getOutputStream(), AcData.class).sheet("模板").doWrite(acDataList);
-        } catch (IOException e) {
-            e.printStackTrace();
-            log.info("导出excel失败{}", e.getMessage());
-            response.reset();
-            response.setContentType("application/json");
-            response.setCharacterEncoding("utf-8");
-            response.getWriter().println("下载文件失败: " + e.getMessage());
-        }
+    @PostMapping("/excel/dc_summary_data")
+    public void downloadDcSummaryData(@RequestBody LocalDate date, HttpServletResponse response) throws IOException {
+        excelService.writeDcSummaryByDate(date, response.getOutputStream());
     }
 }
