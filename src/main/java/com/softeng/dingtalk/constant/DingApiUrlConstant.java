020912a576b64a9d497f56f648c50f1e4ede3ae3
lilingj <1020305202@qq.com>
2022-01-24 22:05:50 +0800

    feat: 每周日定时提醒周报、绩效填写

---
+++ src/main/java/com/softeng/dingtalk/constant/DingApiUrlConstant.java
@@ -0,0 +1,13 @@
+package com.softeng.dingtalk.constant;
+
+/**
+ * 钉钉接口的url
+ * @author lilingj
+ * @date 2022/1/24
+ */
+public class DingApiUrlConstant {
+    /**
+     * 链接消息接口
+     */
+    public static final String LINK_MESSAGE_API_URL = "https://oapi.dingtalk.com/topapi/message/corpconversation/asyncsend_v2";
+}
---
+++ src/main/java/com/softeng/dingtalk/constant/LocalUrlConstant.java
@@ -0,0 +1,11 @@
+package com.softeng.dingtalk.constant;
+
+/**
+ * 本应用的路由，包括前端、后端，供发送消息时设置回调
+ * @author lilingj
+ * @date 2022/1/24
+ */
+
+public class LocalUrlConstant {
+    public static final String FRONTEND_PERFORMANCE_URL = "/performance/perfApplication";
+}
