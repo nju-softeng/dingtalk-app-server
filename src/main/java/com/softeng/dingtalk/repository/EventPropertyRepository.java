a01cdd6a07bcb5b170a4475d98eb0e3716057bd3
夷 巫 <1343296078@qq.com>
2022-03-22 18:13:37 +0800

    feature: get eventProperty by page

--- src/main/java/com/softeng/dingtalk/repository/EventPropertyRepository.java
+++ src/main/java/com/softeng/dingtalk/repository/EventPropertyRepository.java
@@ -2,9 +2,16 @@ package com.softeng.dingtalk.repository;
 
 import com.softeng.dingtalk.entity.EventProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
 
 @Repository
 public interface EventPropertyRepository extends CustomizedRepository<EventProperty, Integer>{
 
+    @Query("select e from EventProperty e")
+    List<EventProperty> findEventByPage(Pageable pageable);
 }
