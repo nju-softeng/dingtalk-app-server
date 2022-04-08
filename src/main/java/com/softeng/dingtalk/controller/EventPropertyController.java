5678c213cc00c2eeaa3c39ab79ca7e9f7862faa1
夷 巫 <1343296078@qq.com>
2022-04-08 17:30:50 +0800

    add some function notes

--- src/main/java/com/softeng/dingtalk/controller/EventPropertyController.java
+++ src/main/java/com/softeng/dingtalk/controller/EventPropertyController.java
@@ -33,7 +33,6 @@ public class EventPropertyController {
         }else{
             eventPropertyService.updateEventProperty(eventProperty);
         }
-
     }
 
     @PostMapping("/event/{eventId}/eventFile")
